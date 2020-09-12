4e6e2346ad6f203ab678a5847900b9c4f906137d
Goatland <kagurazakaseiga@gmail.com>
2020-09-13 02:23:51 +0800

    使mod支援GraphicsLib

--- src/data/scripts/srfpModPlugin.java
+++ src/data/scripts/srfpModPlugin.java
@@ -4,6 +4,9 @@ import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 import data.scripts.world.srfpWorldGen;
 import exerelin.campaign.SectorManager;
+import org.dark.shaders.light.LightData;
+import org.dark.shaders.util.ShaderLib;
+import org.dark.shaders.util.TextureData;
 
 public class srfpModPlugin extends BaseModPlugin {
     @Override
@@ -11,12 +14,22 @@ public class srfpModPlugin extends BaseModPlugin {
         //check if lazylib exists
         boolean hasLazyLib = Global.getSettings().getModManager().isModEnabled("lw_lazylib");
         if (!hasLazyLib) {
-            throw new RuntimeException("SRFP requires LazyLib!");
+            throw new RuntimeException("星际革命:红与黑需要LazyLib!");
         }
         boolean hasMagicLib = Global.getSettings().getModManager().isModEnabled("MagicLib");
         if (!hasMagicLib) {
-            throw new RuntimeException("SRFP requires MagicLib!" +
-                    "\nGet it at http://fractalsoftworks.com/forum/index.php?topic=13718.0");
+            throw new RuntimeException("星际革命:红与黑需要MagicLib!" +
+                    "\n请前往 http://fractalsoftworks.com/forum/index.php?topic=13718.0下载");
+        }
+        boolean hasGraphicsLib = Global.getSettings().getModManager().isModEnabled("shaderLib");
+        if (hasGraphicsLib) {
+            ShaderLib.init();
+            LightData.readLightDataCSV("data/lights/srfp_light_data.csv");
+            TextureData.readTextureDataCSV("data/lights/srfp_texture_data.csv");
+        }
+        if (!hasGraphicsLib) {
+            throw new RuntimeException("星际革命:红与黑需要GraphicsLib!" +
+                    "\n请前往 http://fractalsoftworks.com/forum/index.php?topic=10982下载");
         }
     }
     @Override
